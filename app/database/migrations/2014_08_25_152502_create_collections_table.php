4c65e56ce2c897783b8ff9d4361704cfd57aa61f
kidshenlong <michaelpm91@googlemail.com>
2014-09-14 23:36:51 +0100

    Update collection migration. Cascading deletes.

--- app/database/migrations/2014_08_25_152502_create_collections_table.php
+++ app/database/migrations/2014_08_25_152502_create_collections_table.php
@@ -16,7 +16,7 @@ class CreateCollectionsTable extends Migration {
 		{
 			$table->increments('id');
             $table->integer('upload_id')->length(10)->unsigned();
-            $table->foreign('upload_id')->references('id')->on('uploads');
+            $table->foreign('upload_id')->references('id')->on('uploads')->onDelete('cascade');
             $table->text('collection_contents');
             $table->string('collection_hash');
             $table->integer('collection_status');
