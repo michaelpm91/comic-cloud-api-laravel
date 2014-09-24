b80aadbb054fa1663796110d713bc1e4b1248830
kidshenlong <michaelpm91@googlemail.com>
2014-09-24 19:50:20 +0100

    Lots of Changes. API now uses OAuth 2 instead of Basic Auth. Implemented in most places. Changing upload controller and collectin controller so more of the matching isdone on on client side so the API just handles validation of this information.

---
+++ app/database/migrations/2014_09_24_182851_add_match_data_to_uploads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddMatchDataToUploadsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('uploads', function(Blueprint $table)
+		{
+			
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('uploads', function(Blueprint $table)
+		{
+			
+		});
+	}
+
+}
