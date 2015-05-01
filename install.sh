31ba4981655d4341bc4222fc10412b4d2b9daa90
kidshenlong <michaelpm91@googlemail.com>
2015-05-01 13:37:23 +0100

    Adding change directory to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#cd /var/www/api.comiccloud.io
-#curl -sS https://getcomposer.org/installer | php
-#php composer.phar install
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+ls
+cd /var/www/api.comiccloud.io/
 composer install
