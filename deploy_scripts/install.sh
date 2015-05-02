2df330f9d5c149abc7418ffece053b7629a0ce7b
kidshenlong <michaelpm91@googlemail.com>
2015-05-02 20:36:49 +0100

    Spacing in install script.

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -2,9 +2,14 @@
 #Permissions
 chown -R ec2-user /var/www/api.dev.comiccloud.io/
 chmod -R 775 /var/www/api.dev.comiccloud.io/storage/
+
 #Composer Install
 cd /var/www/api.dev.comiccloud.io/
 COMPOSER_HOME="~/" /usr/local/bin/composer install
+
+#Generate Environment Variables
+php /var/www/api.dev.comiccloud.io/artisan genenvvar --env =  develop
+
 #Supervisor Queue Scripts
 echo -e '#!/bin/bash \n php /var/www/api.dev.comiccloud.io/artisan queue:listen --timeout=0' > /usr/local/run_queue.sh
 chmod +x /usr/local/run_queue.sh
