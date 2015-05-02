1fa213caf3c024ca47a5513e9e46678dca20c849
kidshenlong <michaelpm91@googlemail.com>
2015-05-02 18:47:42 +0100

    develop cloud formation now adjusts php ini file. Install script also changes permissions on folders.

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+chown -R ec2-user /var/www/api.dev.comiccloud.io/
+chmod -R 775 /var/www/api.dev.comiccloud.io/storage/
 cd /var/www/api.dev.comiccloud.io/
 COMPOSER_HOME="~/" /usr/local/bin/composer install
