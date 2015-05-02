b47d1d35f4c760bf85be0eb4b0b44bf82a4bc782
kidshenlong <michaelpm91@googlemail.com>
2015-05-02 20:29:19 +0100

    develop cloud formation now adjusts php ini file as well as python dependencies. Install script also changes permissions on folders and sets up queueing scripts.

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+#Permissions
 chown -R ec2-user /var/www/api.dev.comiccloud.io/
 chmod -R 775 /var/www/api.dev.comiccloud.io/storage/
+#Composer Install
 cd /var/www/api.dev.comiccloud.io/
 COMPOSER_HOME="~/" /usr/local/bin/composer install
+#Supervisor Queue Scripts
+echo -e '#!/bin/bash \n php /var/www/api.dev.comiccloud.io/artisan queue:listen --timeout=0' > /usr/local/run_queue.sh
+chmod +x /usr/local/run_queue.sh
+cat > /etc/supervisor/conf.d/laravel_queue.conf <<EOF
+[program:laravel_queue]
+command=/usr/local/bin/run_queue.sh
+autostart=true
+autorestart=true
+stderr_logfile=/var/log/laraqueue.err.log
+stdout_logfile=/var/log/laraqueue.out.log
+EOF
+supervisorctl reread
+supervisorctl update
