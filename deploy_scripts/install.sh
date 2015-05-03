c3b1136c9e6fe15d3bbb8c6307b9744849b3bc43
kidshenlong <michaelpm91@googlemail.com>
2015-05-03 22:02:13 +0100

    Removed python pip install dependency as cloud formation yum cannot find it. PIP install's version of supervisor does not install supervisor conf file to the location on other distributions like ubuntu. So these directories have been manually created and populated.

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -7,10 +7,15 @@ chmod -R 775 /var/www/api.dev.comiccloud.io/storage/
 cd /var/www/api.dev.comiccloud.io/
 COMPOSER_HOME="~/" /usr/local/bin/composer install
 
+#Reset Database
+#php /var/www/api.dev.comiccloud.io/artisan migrate:refresh
+
 #Generate Environment Variables
 php /var/www/api.dev.comiccloud.io/artisan genenvvar --env =  develop
 
 #Supervisor Queue Scripts
+mkdir /etc/supervisor
+mkdir /etc/supervisor/conf.d
 echo -e '#!/bin/bash \n php /var/www/api.dev.comiccloud.io/artisan queue:listen --timeout=0' > /usr/local/run_queue.sh
 chmod +x /usr/local/run_queue.sh
 cat > /etc/supervisor/conf.d/laravel_queue.conf <<EOF
