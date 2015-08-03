43dd469c050d3f3036855c3b848500ba1de1b8eb
kidshenlong <michaelpm91@googlemail.com>
2015-08-04 00:43:18 +0100

    Created Access Log template for middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,8 @@ class Kernel extends HttpKernel {
 		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
-		//'App\Http\Middleware\VerifyCsrfToken', moved to $routeMiddleware[]
-        //'LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware',
-        'App\Http\Middleware\OAuthExceptionHandlerMiddleware'
+        'App\Http\Middleware\OAuthExceptionHandlerMiddleware',
+        'App\Http\Middleware\AccessLog'
 	];
 
 	/**
