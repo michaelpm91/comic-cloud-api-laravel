14f6f6a962300aec628149c1e4ccaa5430950a73
kidshenlong <michaelpm91@googlemail.com>
2015-08-04 01:14:32 +0100

    Outputting to logentries.

--- app/Http/Middleware/AccessLog.php
+++ app/Http/Middleware/AccessLog.php
@@ -4,6 +4,11 @@ namespace App\Http\Middleware;
 
 use Closure;
 
+use Monolog\Logger;
+use Monolog\Handler\LogEntriesHandler;
+
+use Request;
+
 class AccessLog
 {
     /**
@@ -15,6 +20,24 @@ class AccessLog
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
+
+
+        $logentriesHandler = new LogEntriesHandler(env('LOG_ENTRIES_KEY'));
+        $channel = "Access";
+        $monolog = new Logger($channel);
+        $monolog->pushHandler($logentriesHandler);
+        $monolog->addInfo('This is a info logging message', [
+            'version' => env('APP_API_VERSION'),
+            'route' => $request->path(),
+            'method' => $request->method()
+        ]);
+        $monolog->addWarning('This is a warning logging message', [
+            'version' => env('APP_API_VERSION'),
+            'route' => $request->path(),
+            'method' => $request->method()
+        ]);
+
+        return $response;
     }
 }
