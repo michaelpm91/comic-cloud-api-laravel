565c057149e9047278e850833bb1df94cdd47b37
kidshenlong <michaelpm91@googlemail.com>
2015-08-12 19:08:49 +0100

    Removed supervisor dependencies.

--- infrastructure/aws/code_deploy/install.sh
+++ infrastructure/aws/code_deploy/install.sh
@@ -12,17 +12,3 @@ COMPOSER_HOME="~/" /usr/local/bin/composer install
 
 #Generate Environment Variables
 php /var/www/api.dev.comiccloud.io/artisan genenvvar --env =  develop
-
-#Supervisor Queue Scripts
-echo -e '#!/bin/bash \n php /var/www/api.dev.comiccloud.io/artisan queue:listen --timeout=0' > /usr/local/bin/run_queue.sh
-chmod +x /usr/local/bin/run_queue.sh
-cat > /etc/supervisor/conf.d/laravel_queue.conf <<EOF
-[program:laravel_queue]
-command=/usr/local/bin/run_queue.sh
-autostart=true
-autorestart=true
-stderr_logfile=/var/log/laraqueue.err.log
-stdout_logfile=/var/log/laraqueue.out.log
-EOF
-/usr/local/bin/supervisorctl reread
-/usr/local/bin/supervisorctl update
